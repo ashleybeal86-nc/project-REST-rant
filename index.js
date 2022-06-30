8974d85a08382278c1e508552e29c3e9dbea55d8
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-29 20:19:03 -0400

    adding stuff...IDK

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
 require('dotenv').config()
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.listen(process.env.PORT);
-
-//adding 404 route
-require("dotenv").config();
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
-});
-
-app.listen(process.env.PORT);
-
-//adding controllers
-require('dotenv').config()
 const express = require('express')
 const app = express()
 
