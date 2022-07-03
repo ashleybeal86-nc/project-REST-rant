d0fc6767f1010e1cdec572bd5cddec003ce697a8
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-03 12:45:22 -0400

    added photo to home page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img src="/images/foodpicture1resized.jpg"/>
+          <div>
+            Photo by <a href="Ahttps://unsplash.com/@lvnatikk">Lily Banse</a> on <a href="https://unsplash.com/@lvnatikk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </div>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
