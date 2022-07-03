86508cb90c80d4b34add0ff13f42af99387938bb
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-03 14:28:23 -0400

    added form for adding a new place

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/new", (req, res) => {
-  res.render("places/new");
+  res.render("places/new")
 });
 
 router.get("/", (req, res) => {
@@ -23,5 +23,6 @@ router.get("/", (req, res) => {
   ];
   res.render("places/index", { places });
 });
+
 
 module.exports = router;
