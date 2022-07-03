88dc9633f7f2aab6356c7de2f1728c024f80fa22
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-03 11:55:32 -0400

    fixed changes with places.js and index.jsx files

--- index.js
+++ index.js
@@ -7,32 +7,13 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 app.use("/places", require("./controllers/places"));
 
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("*", (req, res) => {
   res.render("error404");
-});
-
-app.get("/", (req, res) => {
-  res.render("places/index", { places });
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "http://placekitten.com/250/250",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "http://placekitten.com/250/250",
-    },
-  ];
 });
 
 app.listen(process.env.PORT);
