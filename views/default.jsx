59cc4a497750ff52771bade6697abea578c81760
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-03 12:05:25 -0400

    adding public folder and css

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,10 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        />
       </head>
       <body>{html.children}</body>
     </html>
