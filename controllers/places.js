c6774c2ee7a84b2aa071694c074f8db557ec18f7
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-05 12:43:37 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,27 @@
 const router = require("express").Router();
+const places = require("../models/places.js")
 
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/restaurantpic1.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Miami",
-      state: "FL",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/restaurantpic2.jpg",
-    },
-  ];
   res.render("places/index", { places });
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("POST /places");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body)
+  res.redirect("/places");
 });
-
 
 module.exports = router;
