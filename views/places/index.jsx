b9fab7111355d1bb16bf484448bd7817c4814c78
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-17 11:28:41 -0400

    fixed display issue with places

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,25 +2,20 @@ const React = require("react");
 const Def = require("../default");
 
 function index(data) {
-   let placesFormatted = data.places.map((place) => {
-  return (
-    <div className="col-sm-6">
-      <h2>
-        <a href={'/places/${place.id}'} >
-          {place.name}
-        </a>
-      </h2>
-      <p className="text-center">
-        {place.cuisines}
-      </p>
-      <img src={place.pic} alt={place.name} />
-      <p className="text-center">
-        Located in {place.city}, {place.state}
-      </p>
-    </div>
-  )
-})
-
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div className="col-sm-6">
+        <h2>
+          <a href={`/places/${place.id}`}>{place.name}</a>
+        </h2>
+        <p className="text-center">{place.cuisines}</p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    );
+  });
   return (
     <Def>
       <main>
@@ -30,6 +25,5 @@ function index(data) {
     </Def>
   );
 }
-
 
 module.exports = index;
